eeea4b4ae51469bea0ecd78dd0b926eabdcf8a47
angelruiz <angeldavid29121990@gmail.com>
2019-12-14 21:19:02 -0600

    working in methods with return.

--- MethodsWithReturn.java
+++ MethodsWithReturn.java
@@ -13,3 +13,13 @@ protect Boolean trueOrFalse(Boolean comparation){
 
 return comparation;
 
+private String tellHistory(String historyName){
+
+String newHistory =
+ historyName + "En ocasiones, las cosas que nos hacen realmente felices pasan por nuestra vida sin nisiquiera darnos cuenta. 
+ Querernos a nosotros mismos incluye valorar estas cosas o personas que nos rodean y que, por distintos motivos, podemos no tomarlas en cuenta. 
+ De esta manera nos arrepentiremos en el futuro."
+
+return newHistory;
+}
+
