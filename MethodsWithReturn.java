da7b1a49fc9e6da994c6bd0f0bd7947fd9fdd66a
angelruiz <angeldavid29121990@gmail.com>
2019-12-14 20:36:21 -0600

    new file, for work with methods with return and that receibe parameters.

---
+++ MethodsWithReturn.java
@@ -0,0 +1,15 @@
+
+private int addNumbers(int a, int b){
+
+return a + b;
+}
+
+public String joinStrings(String nameUser){
+
+return nameUser + "Angel";
+}
+
+protect Boolean trueOrFalse(Boolean comparation){
+
+return comparation;
+
