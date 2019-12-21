d3d0c3897c470c897ab8474d79f38907c9165915
angelruiz <angeldavid29121990@gmail.com>
2019-12-21 17:57:49 -0600

    working in method tellHistory...

--- MethodsWithReturn.java
+++ MethodsWithReturn.java
@@ -20,6 +20,9 @@ String newHistory =
  Querernos a nosotros mismos incluye valorar estas cosas o personas que nos rodean y que, por distintos motivos, podemos no tomarlas en cuenta. 
  De esta manera nos arrepentiremos en el futuro."
 
+int numbers = "1, 2, 3, 4, 5, 6, 7, 8, 9, 10";
+numbers * 100;
+
 return newHistory;
 }
 
